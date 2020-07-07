d8421e92cb470e39433030e00434d9b69702920f
kelbri10 <kelbri10@gmail.com>
2020-07-07 01:19:44 -0400

    added pseudo code to script.js and framework to index.html

---
+++ script.js
@@ -0,0 +1,6 @@
+//when user opens the planner, the current day is displayed at the top of the calender
+//when user scrolls down, user sees timeblocks for standard business hours
+//when user views timeblocks, each timblock indicates past, present, or future
+//when user clicks a timeblock, user enters an event
+//when user clicks save button for timeblock the text is saved for that timeblock
+//when user refreshes the page, the saved event is still existing
