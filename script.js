fe38c627e5c7845d25879456166a079e70065c1a
kelbri10 <kelbri10@gmail.com>
2020-07-26 13:54:56 -0400

    added compareTime function to compare iterator to present time

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let newDay = moment().format("MMM Do YYYY");
 $('#currentDay').text(newDay); 
 
 //when user scrolls down, user sees timeblocks for standard business hours - 9am - 5pm 
-function createPlanner(){
+const createPlanner = () =>{
     for(let i = 9; i <= 17; i++){  
 
         let timeBlock = document.createElement('div'); 
@@ -19,11 +19,7 @@ function createPlanner(){
 
         hour.setAttribute('class', 'col-md-2 hour'); 
 
-        event.setAttribute('class', 'col-md-8 description');
-
-        //if (time === moment().get('hour')){ 
-        //    event.setAttribute('class', 'present'); 
-        //}
+        compareTime(i, event); 
 
         saveBtn.setAttribute('class', 'col-md-2 saveBtn fas fa-save'); 
     
@@ -34,14 +30,21 @@ function createPlanner(){
     }
 }
 
-//when user views timeblocks, each timblock indicates past, present, or future
-//compare to current hr 
+const compareTime = (i, event) =>{
+        //compares iterator to the current hour to determine color appears
+        //to user 
+        let present = moment().get('hour'); 
 
-//if hour is before, display color gray
+        //present displays red, past displays gray, future displays green
+        if(i === present){ 
+            event.setAttribute('class', 'col-md-8 description present'); 
+        } else if (i < present){ 
+            event.setAttribute('class', 'col-md-8 description past');
+        } else {
+            event.setAttribute('class', 'col-md-8 description future'); 
+        }
+}
 
-//if hour is equal, display color red 
-
-//if hour is after, display color green 
 
 //when user clicks a timeblock, user enters an event
 //on click add event listener allow user to type in text box 
@@ -53,3 +56,5 @@ function createPlanner(){
 
 createPlanner(); 
 console.log(moment()); 
+let present = moment().get('hour'); 
+        console.log(present); 
